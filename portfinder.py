db3d8f6388776271e1ec6c31a1119ff5ca87237a
Hi-iamsolo <105360134+Hi-iamsolo@users.noreply.github.com>
2022-06-02 21:13:51 +0545

    Add files via upload

--- portfinder.py
+++ portfinder.py
@@ -1,11 +1,14 @@
 import requests as rq
-
+print('............PORT FINDER...........')
 ip=input('enter ip:')
 print('1.http\n2.https')
-pro=input('Choose protocol:')
+pro=input('Choose protocol:\n')
+print('Enter port range')
+rang=input('From:')
+rang2=input('To:')
 	
 if pro==str(1):
-		for port in range(7999,8090):
+		for port in range(int(rang),int(rang2)):
 			try:
 				rq.get('http://'+ip+':'+str(port))
 				print('http://'+ip+':'+str(port))
@@ -31,7 +34,3 @@ else:
 	
 	
 	
-
-	
-	
-
